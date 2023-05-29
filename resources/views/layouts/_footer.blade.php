5a573e450e1f2dfd86f2555ed5522cf0926ea9c1
hr10282018 <79360611+hr10282018@users.noreply.github.com>
2023-05-29 16:39:19 +0800

    update

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -6,7 +6,7 @@
     <p class="float-left">
       由 <a href="" target="_blank">Sakura</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
-    <p class="float-right"><a href="https://www.xujc.com/">联系我们</a></p>
+    <p class="float-right"><a href="">联系我们</a></p>
   </div>
 </footer>
 
